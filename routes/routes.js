480e703852446933d27902e49f14fd8329734dc8
ezoltech <ezramamo13@gmail.com>
2023-10-15 11:57:59 +0300

    iniitalizing controllers

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
 const router = require("express").Router();
-
+const adminController = require("../controllers/adminController");
+const advertController = require("../controllers/advertController");
 module.exports = router;
